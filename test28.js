722a1f83ebbbdb71153ac5b53237b851ef49e4f0
zhaoyiming <yiming_zhao@qingsongchou.com>
2018-07-31 15:45:52 +0800

    script async

---
+++ test28.js
@@ -0,0 +1,39 @@
+;(function () {
+
+    console.log('script start');
+
+    function isArray (field) {
+        return Object.prototype.toString.call(field) === '[object Array]';
+    }
+
+    function isFunc (field) {
+        return Object.prototype.toString.call(field) === '[object Function]';
+    }
+
+    function forEachAsync (arr, callback) {
+        if (!isArray(arr)) {
+            throw new Error('arr must be a Array');
+        }
+
+        if (!isFunc(callback)) {
+            throw new Error('callback must be a Function');
+        }
+
+        var timer = setInterval(function () {
+            if (arr.length === 0) {
+                clearInterval(timer);
+                timer = null;
+            } else {
+                callback(arr.shift());
+            }
+        }, 4);
+    }
+
+    var arr = [1, 2, 3, 4, 5];
+    forEachAsync(arr, function (item) {
+        console.log(item);
+    });
+
+    console.log('script end');
+
+})();
