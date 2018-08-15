4c1a55dcad2720c7b907fe3a7af444ffa79d350c
zhaoyiming <yiming_zhao@qingsongchou.com>
2018-08-15 17:03:03 +0800

    diviceByBASE

---
+++ test43.js
@@ -0,0 +1,20 @@
+;(function () {
+
+    function diviceByBase (num, base) {
+        var arr = [];
+        var str = '';
+        var rem = 0;
+        var baseStr = '0123456789ABCDEF';
+
+        while (num > 0) {
+            rem = num % base;
+            arr.push(baseStr[rem]);
+            num = Math.floor(num / base);
+        }
+
+        return arr.reverse().join('');
+    }
+
+    console.log(diviceByBase(2, 2));
+
+})();
