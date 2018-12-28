28aa462ca4040a4c4acb8f0c2b47830aa12e3577
zhaoyiming <yiming_zhao@qingsongchou.com>
2018-12-28 14:42:48 +0800

    cache by closure

---
+++ test57.js
@@ -0,0 +1,23 @@
+;(function () {
+
+  function cached (fn) {
+    var cache = Object.create(null);
+    return function (str) {
+      const hit = cache[str];
+      debugger;
+      return hit || (cache[str] = fn(str));
+    }
+  }
+
+  var reg = /-(\w)/g;
+
+  var camelize = cached(function (str) {
+    return str.replace(reg, function ($0, $1) {
+      return $1 ? $1.toUpperCase() : '';
+    });
+  });
+
+  console.log(camelize('hello-world'));
+  console.log(camelize('hello-zym'));
+  
+})();
