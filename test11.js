c05e696787baeb50df84428aa510ccf03ee29a72
zhaoyiming <yiming_zhao@qingsongchou.com>
2019-04-15 16:12:42 +0800

    chore: optimize deepCopy function

--- test11.js
+++ test11.js
@@ -1,44 +1,42 @@
 ;(function () {
 
-    var obj1 = {
-        name: 'zhangsan',
-        age: 18
+    var person = {
+      name: 'zymfe',
+      age: 18,
+      skills: ['HTML', 'CSS', 'JavaScript'],
     };
-
-    var obj2 = {
-        name: 'lisi',
-        sex: 'man',
-        work: [1, 2, 3],
-        love: {
-            item1: '1',
-            item2: '2',
-            item3: '3',
-            item4: [0, 1, 2, 3]
+  
+    function deepCopy (origin, target) {
+      var origin = origin || {};
+      var keys = Object.keys(target);
+  
+      for (var i = 0; i < keys.length; i += 1) {
+        var key = keys[i];
+        
+        if (origin[key]) {
+          continue;
         }
-    };
-
-    Object.prototype.deepCopy = function (target) {
-        for (var prop in target) {
-            if (this[prop]) continue;
-            if (typeof target[prop] !== 'object') {
-                this[prop] = target[prop];
-                continue;
-            }
-            var type = Object.prototype.toString.call(target[prop]);
-            this[prop] = type === '[object Array]'
-                ? []
-                : type === '[object Object]'
-                ? {}
-                : function () {};
-            this[prop].deepCopy(target[prop]);
+  
+        var val = target[key];
+        if (typeof val !== 'object') {
+          origin[key] = val;
+          continue;
         }
+  
+        var child = Array.isArray(val)
+          ? []
+          : {};
+        origin[key] = deepCopy(child, val);
+      }
+  
+      return origin;
     }
-
-    obj1.deepCopy(obj2);
-    obj1.name = 'wangwu';
-    obj1.work[2] = 'haha';
-    obj1.love.item4.push(4);
-    console.log(obj1);
-    console.log(obj2);
-
-})();
+  
+    var copiedPerson = deepCopy({}, person);
+    copiedPerson.age = 20;
+    copiedPerson.skills.push('NodeJs');
+  
+    console.log(copiedPerson);
+    console.log(person);
+  
+  })();
