da1fa2a51d048cfe014be4764aac2512c544cc7f
zhaoyiming <yiming_zhao@qingsongchou.com>
2019-04-15 15:16:48 +0800

    chore: optimize quickSort function

--- test10.js
+++ test10.js
@@ -1,27 +1,35 @@
 ;(function () {
 
-    console.time('time');
+	var arr = [1, 100, -99, 10, 22, 33, 0];
 
-    var arr = [1, 100, -99, 10, 22, 33, 0];
+	function quickSort(arr) {
+		if (arr.length <= 1) {
+			return arr;
+		}
 
-    function quickSort (arr) {
-        if (arr.length <= 1) return arr;
-        var left = [];
-        var right = [];
-        var middle = arr[0];
-        for (var i = 1; i < arr.length; i += 1) {
-            if (arr[i] <= middle) {
-                left.push(arr[i]);
-            } else {
-                right.push(arr[i]);
-            }
-        }
-        var _left = quickSort(left);
-        var _right = quickSort(right);
-        return _left.concat(middle, _right);
-    }
+		var left = [];
+		var right = [];
+		var middle = arr[0];
 
-    console.log(quickSort(arr));
-    console.timeEnd('time')
+		for (var i = 1; i < arr.length; i += 1) {
+			var cur = arr[i];
+
+			if (cur <= middle) {
+				left.push(cur);
+			} else {
+				right.push(cur);
+			}
+		}
+
+		var _left = quickSort(left);
+		var _right = quickSort(right);
+
+		return _left.concat(middle, _right);
+	}
+
+	console.time('time');
+	var res = quickSort(arr);
+	console.timeEnd('time');
+	console.log(res);
 
 })();
