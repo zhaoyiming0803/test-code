632f53fe5751453f4e1832c371e5c9c59127d672
zhaoyiming <yiming_zhao@qingsongchou.com>
2019-04-15 16:18:19 +0800

    update: operators

--- test12.js
+++ test12.js
@@ -3,7 +3,13 @@
     var a = 0;
     var b = a > 5 && 3 || 4 && 5;
     console.log(b);
-
-    console.log(this);
-
-})();
+  
+    var c = 3;
+    var d = c > 1 && 5;
+    console.log(d);
+  
+    var e = 5;
+    var f = e > 0 && e < 10;
+    console.log(f);
+  
+  })();
