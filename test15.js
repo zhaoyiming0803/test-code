66a922900456d292882563e7b40f870ad1ef495a
zhaoyiming <yiming_zhao@qingsongchou.com>
2019-04-15 16:56:14 +0800

    chore: optimize publish and subscribe mode

--- test15.js
+++ test15.js
@@ -1,37 +1,42 @@
 ;(function () {
 
-    function isFunc (func) {
-        return Object.prototype.toString.call(func) === '[object Function]';
-    }
+	function isFunc(func) {
+		return Object.prototype.toString.call(func) === '[object Function]';
+	}
 
-    function Event () {
-        this.eventList = {};
-    }
+	function Event() {
+		this.eventMap = {};
+	}
 
-    Event.prototype.on = function (eventName, callback) {
-        if (this.eventList[eventName] === undefined) {
-            this.eventList[eventName] = [];
-        }
-        if (!isFunc(callback)) {
-            throw new Error('callback must be a function');
-        }
-        this.eventList[eventName].push(callback);
-    }
+	Event.prototype.on = function (eventName, callback) {
+		if (this.eventMap[eventName] === undefined) {
+			this.eventMap[eventName] = [];
+		}
 
-    Event.prototype.emit = function (eventName) {
-        if (this.eventList[eventName] === undefined) {
-            throw new Error('[' + eventName + '] is not defined');
-        }
-        var args = [].slice.call(arguments, 1);
-        for (var i = 0; i < this.eventList[eventName].length; i += 1) {
-            this.eventList[eventName][i].apply(this, args);
-        }
-    }
+		if (!isFunc(callback)) {
+			throw new Error('callback must be a function');
+		}
+		
+		this.eventMap[eventName].push(callback);
+	}
 
-    var a = new Event();
-    a.on('msg', function (a, b, c) {
-        console.log(a, b, c);
-    });
-    a.emit('msg', 1, 2, 3);
+	Event.prototype.emit = function (eventName) {
+		var eventCbs = this.eventMap[eventName];
+
+		if (eventCbs === undefined) {
+			throw new Error('[' + eventName + '] is not defined');
+		}
+
+		var args = [].slice.call(arguments, 1);
+		for (var i = 0; i < eventCbs.length; i += 1) {
+			eventCbs[i].apply(this, args);
+		}
+	}
+
+	var a = new Event();
+	a.on('msg', function (a, b, c) {
+		console.log(a, b, c);
+	});
+	a.emit('msg', 1, 2, 3);
 
 })();
