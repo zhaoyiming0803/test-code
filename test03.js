3c768084e3cbda372c1d7900ae80ce3c8987a805
zhaoyiming <yiming_zhao@qingsongchou.com>
2019-04-15 15:32:24 +0800

    chore: optimize single mode

--- test03.js
+++ test03.js
@@ -1,27 +1,28 @@
 ;(function () {
 
     function Person (name) {
-        this.name = name;
+      this.name = name;
     }
-
+  
     Person.prototype.showName = function () {
-        return this.name;
+      return this.name;
     }
-
+  
     var ProxyPerson = (function () {
-        var instance = null;
-        return function (name) {
-            if (instance === null) {
-                instance = new Person(name);
-            }
-            return instance;
+      var instance = null;
+  
+      return function person (name) {
+        if (instance === null) {
+          instance = new Person(name);
         }
+        return instance;
+      }
     })();
-
+  
     var p1 = new ProxyPerson('zhangsan');
     var p2 = new ProxyPerson('lisi');
-
+    
     console.log(p1.showName());
     console.log(p2.showName());
-
-})();
+  
+  })();
