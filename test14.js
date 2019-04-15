780cde5b77610647c265a2689e8de422b9798d6a
zhaoyiming <yiming_zhao@qingsongchou.com>
2019-04-15 16:48:04 +0800

    chore: reset array __proto__

--- test14.js
+++ test14.js
@@ -1,27 +1,28 @@
 ;(function () {
+
+    var arr1 = ['a', 'b', 'c'];
+    var arr2 = [0, 1, 2];
   
-    var arr1 = [1, 2, 3, 4];
-    var arr2 = ['a', 'b', 'c'];
-
-    var arrList = ['push', 'pop', 'unshift', 'shift', 'reverse', 'splice', 'slice', 'join'];
-
     var arrProto = {};
-
-    for (var i = 0; i < arrList.length; i += 1) {
-        arrProto[arrList[i]] = (function (i) {
-            return function () {
-                console.log('hello world');
-                return [][arrList[i]].apply(this, arguments);
-            }
-        })(i);
+    var arrMethodList = ['push', 'pop', 'unshift', 'shift', 'splice', 'slice', 'reverse', 'sort', 'concat', 'join'];
+  
+    for (var i = 0; i < arrMethodList.length; i += 1) {
+      arrProto[arrMethodList[i]] = (function (i) {
+        return function () {
+          console.log('做自己想做的事情');
+          var args = [].slice.call(arguments, 0);
+          return [][arrMethodList[i]].apply(this, args);
+        }
+      })(i);
     }
-    
-    arr1.__proto__ = arrProto;
-    
-    arr1.push(5);
-    arr2.push('d');
-
-    console.log(arr1);
+  
+    arr2.__proto__ = arrProto;
+  
+    arr2.push(3);
     console.log(arr2);
-
-})();
+    console.log(Object.prototype.toString.call(arr2));
+  
+    arr1.push('d');
+    console.log(arr1);
+  
+  })();
