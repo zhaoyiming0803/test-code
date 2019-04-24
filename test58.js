b712d998dc03a979158cc16c6741ef7042164c11
zhaoyiming <yiming_zhao@qingsongchou.com>
2019-04-24 17:39:55 +0800

    update: es6 proxy

--- test58.js
+++ test58.js
@@ -9,6 +9,12 @@
     _skill: ['HTML', 'CSS', 'JavaScript']
   };
 
+  person.__proto__ = {
+    showName: function () {
+      return this.name;
+    }
+  };
+
   var proxy = new Proxy(person, {
     get: function (target, key) {
       var value = target[key];
@@ -37,8 +43,16 @@
   console.log(proxy.sex);
   console.log(proxy.age);
 
+  // has方法拦截的是HasProperty操作，而不是HasOwnProperty操作，即has方法不判断一个属性是对象自身的属性，还是继承的属性
+  // has拦截只对in运算符生效，对for...in循环不生效，导致不符合要求的属性没有被for...in循环所排除。
   console.log('_name' in proxy);
   console.log('hehe' in proxy);
+  console.log('name' in proxy);
+  console.log('prototype: ', proxy.showName);
+
+  var obj = Object.create(proxy);
+  console.log('obj.name: ', obj.name); // zhaoyiming
+  console.log('obj.love: ', obj.love); // property or method 'love' is not defined
 
   var fn = function () {
     return 'I am the fn';
@@ -46,14 +60,15 @@
 
   var proxyFn = new Proxy(fn, {
     apply: function (target, ctx, args) {
-      console.log(target);
-      console.log(ctx);
-      console.log(args);
+      console.log('apply-target: ', target);
+      console.log('apply-ctx: ', ctx);
+      console.log('apply-args: ', args);
       return 'I am the proxyFn';
     }
   });
 
   console.log(proxyFn.call(person, 1, 2, 3));
   console.log(proxyFn.apply(person, [4, 5, 6]));
+  console.log(proxyFn({}));
   
 })();
