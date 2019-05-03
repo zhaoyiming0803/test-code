5097f33cd26f8e3e4defbc65982b1a5c9f3b9d5c
zhaoyiming <yiming_zhao@qingsongchou.com>
2019-05-03 11:38:13 +0800

    update: delete duplicate codes

--- test59.js
+++ test59.js
@@ -1,55 +1,5 @@
-; (function () {
+;(function () {
 
-  function deepCopy(target, from) {
-    for (var prop in from) {
-      if (!from.hasOwnProperty(prop) || target[prop]) {
-        continue;
-      }
-
-      var type = Object.prototype.toString.call(from[prop]);
-
-      if (type === '[object Object]') {
-        target[prop] = {};
-        deepCopy(target[prop], from[prop]);
-      } else if (type === '[object Array]') {
-        target[prop] = [];
-        deepCopy(target[prop], from[prop]);
-      } else {
-        target[prop] = from[prop];
-      }
-    }
-
-    return target;
-  }
-
-  var person = {
-    name: 'zym',
-    age: 18,
-    skills: ['html', 'css', 'javaScript'],
-    love: {
-      fruits: ['apple', 'pear', 'melon'],
-      sport: ['backetball', 'ping-pong']
-    }
-  };
-
-  var obj = deepCopy({}, person);
-
-  obj.love.fruits[2] = 'banana';
-
-  console.log(obj);
-  console.log(person);
-
-
-
-
-  var arr1 = [{ a: 1, b: 2 }, 3, 4, 5];
   
-  var arr2 = deepCopy([], arr1);
-
-  arr2[0].a = 100;
-
-  console.log(arr2);
-  console.log(arr1);
-
 
 })();
