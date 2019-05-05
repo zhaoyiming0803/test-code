92e194a679649ce6ea17123d2be167b92acebec0
zhaoyiming <yiming_zhao@qingsongchou.com>
2019-05-05 18:38:33 +0800

    update: optimize deepCopy function

--- test11.js
+++ test11.js
@@ -1,43 +1,39 @@
 ;(function () {
+  
+    function deepCopy (origin) {
+      var target = Array.isArray(origin) ? [] : {};
 
+      for (var prop in origin) {
+        if (target[prop]) {
+          continue;
+        }
+        var childOrigin = origin[prop];
+        target[prop] = typeof childOrigin !== 'object'
+          ? childOrigin
+          : deepCopy(childOrigin);
+      }
+
+      return target;
+    }
+    
     var person = {
       name: 'zymfe',
       age: 18,
       skills: ['HTML', 'CSS', 'JavaScript'],
     };
-  
-    function deepCopy (target, origin) {
-      if (origin === undefined) {
-        origin = target;
-        target = {};
-      }
-      
-      for (let prop in origin) {
-        if (!origin.hasOwnProperty(prop) || target[prop]) {
-          continue;
-        }
-
-        var originChild = origin[prop];
-
-        if (typeof originChild === 'string') {
-          target[prop] = originChild;
-          continue;
-        }
-
-        target[prop] = Array.isArray(originChild)
-          ? []
-          : {};
-        deepCopy(target[prop], originChild);
-      }
-    
-      return target;
-    }
-  
-    var copiedPerson = deepCopy({}, person);
+    var copiedPerson = deepCopy(person);
     copiedPerson.age = 20;
     copiedPerson.skills.push('NodeJs');
-  
-    console.log(copiedPerson);
     console.log(person);
+    console.log(copiedPerson);
+
+    console.log('------------------');
+
+    var arr = [1, 2, {count: 3}];
+    var copiedArr = deepCopy(arr);
+    copiedArr[1] = 200;
+    copiedArr[2].count = 300;
+    console.log(arr);
+    console.log(copiedArr);
   
   })();
