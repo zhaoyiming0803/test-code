b45f7b1b02fa309b14e75cb5d14381ffa170991b
zhaoyiming <yiming_zhao@qingsongchou.com>
2019-05-05 14:14:06 +0800

    higher order function and function static property

--- test59.js
+++ test59.js
@@ -1,5 +1,31 @@
 ;(function () {
 
-  
+  var fn = (function () {
+    var count = 0;
+    return function () {
+      return count += 1;
+    }
+  })();
+
+  console.log(fn());
+  console.log(fn());
+  console.log(fn());
+  console.log(fn());
+  console.log(fn());
+  console.log(fn());
+  console.log('--------');
+
+})();
+
+;(function () {
+
+  function fn() {
+    return fn.count += 1;
+  };
+  fn.count = 0;
+
+  console.log(fn());
+  console.log(fn());
+  console.log(fn());
 
 })();
