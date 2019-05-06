e6c8fa60f8d66a58c03f7c2e521a977029c52197
zhaoyiming <yiming_zhao@qingsongchou.com>
2019-05-06 11:28:27 +0800

    chore: recursion tree

--- test50.js
+++ test50.js
@@ -1,45 +1,41 @@
 ;(function () {
-    
-    var tree = [
-        {
-            id: 1,
-            parent_id: 0,
-            checked: false,
-            children: [
-                {
-                    id: 2,
-                    parent_id: 1,
-                    checked: false
-                },
-                {
-                    id: 3,
-                    parent_id: 1,
-                    checked: false,
-                    children: [
-                         {
-                            id: 4,
-                            parent_id: 3,
-                            checked: true
-                        }
-                    ]
-                }
-            ]
-        }
-    ];
 
-    function handleParent (parent, arr) {
-        for (var i = 0, len = arr.length; i < len; i += 1) {
-            if (Array.isArray(arr[i].children) && arr[i].children.length) {
-                handleParent(arr[i], arr[i].children);
-            }
-            if (parent && arr[i].checked === true) {
-                parent.checked = true;
-            }
-        }
-    }
+	var tree = [{
+		id: 1,
+		parent_id: 0,
+		checked: false,
+		children: [{
+				id: 2,
+				parent_id: 1,
+				checked: false
+			},
+			{
+				id: 3,
+				parent_id: 1,
+				checked: false,
+				children: [{
+					id: 4,
+					parent_id: 3,
+					checked: true
+				}]
+			}
+		]
+	}];
 
-    handleParent(null, tree);
+	function handleParent(parent, arr) {
+		for (var i = 0, len = arr.length; i < len; i += 1) {
+			var cur = arr[i];
+			var child = cur.children;
+			if (Array.isArray(child) && child.length) {
+				handleParent(cur, child);
+			}
+			if (cur.checked === true && parent) {
+				parent.checked = true;
+			}
+		}
+	}
 
-    console.log(tree);
+	handleParent(null, tree);
+	console.log(tree);
 
 })();
