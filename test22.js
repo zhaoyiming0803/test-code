fba28d094491618a4f85c23cd9a77b387ba1a8cd
zhaoyiming <yiming_zhao@qingsongchou.com>
2019-05-21 15:01:25 +0800

    chore: prototype new

--- test22.js
+++ test22.js
@@ -1,34 +1,32 @@
-;(function () {
+// node 没有 window 对象，当前脚本需在浏览器端运行
 
-	function Foo () {
-		getName = function () {
-			console.log(1);
-		}
-		return this;
+function Foo () {
+	getName = function () {
+		console.log(1);
 	}
+	return this;
+}
 
-	Foo.getName = function () {
-		console.log(2);
-	}
+Foo.getName = function () {
+	console.log(2);
+}
 
-	Foo.prototype.getName = function () {
-		console.log(3);
-	}
+Foo.prototype.getName = function () {
+	console.log(3);
+}
 
-	var getName = function () {
-		console.log(4);
-	}
+var getName = function () {
+	console.log(4);
+}
 
-	function getName () {
-		console.log(5);
-	}
+function getName () {
+	console.log(5);
+}
 
-	Foo.getName(); // 2
-	getName(); // 4
-	Foo().getName(); // 1
-	getName(); // 1
-	new Foo.getName(); // 2
-	new Foo().getName(); // 3
-	new new Foo().getName(); // 3
-
-})();
+Foo.getName(); // 2
+getName(); // 4
+Foo().getName(); // 1
+getName(); // 1
+new Foo.getName(); // 2
+new Foo().getName(); // 3
+new new Foo().getName(); // 3
