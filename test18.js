84375847ece590e715aa83f875f850906cac9bd2
zhaoyiming <yiming_zhao@qingsongchou.com>
2019-05-21 14:51:54 +0800

    update: optimize array unique

--- test18.js
+++ test18.js
@@ -1,23 +1,18 @@
 ;(function () {
 
-    var arr = [1, 2, 1, 2, 1, 4, 5, 100, 99, 100, '100', 2, 3, 6, 7];
+	var arr = [1, 2, 1, 2, 1, 4, 5, 100, 99, 100, '100', 2, 3, 6, 7];
 
-    function compare (a, b) {
-        return a > b ? 1 : a < b ? -1 : 0;
-    }
+	function unique (arr) {
+		var sortArr = arr.sort((a, b) => a - b);
+		var newArr = [sortArr[0]];
+		for (var i = 1; i < sortArr.length; i += 1) {
+			if (sortArr[i] !== newArr[newArr.length - 1]) {
+				newArr.push(sortArr[i]);
+			}
+		}
+		return newArr;
+	}
 
-    function unique (arr) {
-        var sortArr = arr.sort(compare);
-        var newArr = [sortArr[0]];
-        for (var i = 1; i < sortArr.length; i += 1) {
-            if (sortArr[i] !== newArr[newArr.length-1]) {
-                newArr.push(sortArr[i]);
-            }
-        }
-        return newArr;
-
-    }
-
-    console.log(unique(arr));
+	console.log(unique(arr));
 
 })();
