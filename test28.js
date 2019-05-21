586c0b360a77ce2351d4ee77110454431d1c9d33
zhaoyiming <yiming_zhao@qingsongchou.com>
2019-05-21 15:07:17 +0800

    chore: format codes of forEachAsync

--- test28.js
+++ test28.js
@@ -1,39 +1,39 @@
 ;(function () {
 
-    console.log('script start');
+	console.log('script start');
 
-    function isArray (field) {
-        return Object.prototype.toString.call(field) === '[object Array]';
-    }
+	function isArray (field) {
+		return Object.prototype.toString.call(field) === '[object Array]';
+	}
 
-    function isFunc (field) {
-        return Object.prototype.toString.call(field) === '[object Function]';
-    }
+	function isFunc (field) {
+		return Object.prototype.toString.call(field) === '[object Function]';
+	}
 
-    function forEachAsync (arr, callback) {
-        if (!isArray(arr)) {
-            throw new Error('arr must be a Array');
-        }
+	function forEachAsync (arr, callback) {
+		if (!isArray(arr)) {
+			throw new Error('arr must be an Array');
+		}
 
-        if (!isFunc(callback)) {
-            throw new Error('callback must be a Function');
-        }
+		if (!isFunc(callback)) {
+			throw new Error('callback must be a Function');
+		}
 
-        var timer = setInterval(function () {
-            if (arr.length === 0) {
-                clearInterval(timer);
-                timer = null;
-            } else {
-                callback(arr.shift());
-            }
-        }, 4);
-    }
+		var timer = setInterval(function () {
+			if (arr.length === 0) {
+				clearInterval(timer);
+				timer = null;
+			} else {
+				callback(arr.shift());
+			}
+		}, 4);
+	}
 
-    var arr = [1, 2, 3, 4, 5];
-    forEachAsync(arr, function (item) {
-        console.log(item);
-    });
+	var arr = [1, 2, 3, 4, 5];
+	forEachAsync(arr, function (item) {
+		console.log(item);
+	});
 
-    console.log('script end');
+	console.log('script end');
 
 })();
