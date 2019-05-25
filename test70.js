99b73a6756e0c203e055370c8909d41906c7818b
zhaoyiming <yiming_zhao@qingsongchou.com>
2019-05-25 22:03:37 +0800

    feature: new Function

---
+++ test70.js
@@ -0,0 +1,12 @@
+// 使用Function构造器生成的函数，并不会在创建它们的上下文中创建闭包；它们一般在全局作用域中被创建。
+// 当运行这些函数的时候，它们只能访问自己的本地变量和全局变量，不能访问Function构造器被调用生成的上下文的作用域。
+// 这和使用带有函数表达式代码的 eval 不同
+
+var n = 1;
+
+function fn () {
+  var n = 2;
+  return new Function('a', 'b', 'return a + b + n;');
+}
+
+console.log(fn()(1, 2));
