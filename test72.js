8cd4b508dedc9d73a61e22d0a2f990757e677a7d
zhaoyiming <yiming_zhao@qingsongchou.com>
2019-05-31 17:57:24 +0800

    test: Promsie setTimeout setImmediate

---
+++ test72.js
@@ -0,0 +1,27 @@
+;(function () {
+
+  setTimeout(function () {
+    console.log(0);
+  });
+
+  setImmediate(function () {
+    console.log(1);
+  });
+
+  console.log(2);
+
+  Promise.resolve().then(function (res) {
+    console.log(3);
+  });
+
+  // 2 3 0 1
+
+  // 优先级顺序：process.nextTick > setTimeout/setInterval > setImmediate
+  // setTimeout需要使用红黑树，且after设置为0，其实会被node强制转换为1，存在性能上的问题，建议替换为setImmediate
+  // process.nextTick有一些比较难懂的问题和隐患，从0.8版本开始加入setImmediate，使用时，建议使用setImmediate
+
+  // 参考文档：
+  // https://www.jianshu.com/p/837b584e1bdd
+  // https://www.zhihu.com/question/23028843/answer/34585034
+  // https://nodejs.org/en/blog/release/v0.10.0/
+})();
