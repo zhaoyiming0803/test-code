8d1149f46c9c87bfc7a0109fef6768716d919e39
zhaoyiming <yiming_zhao@qingsongchou.com>
2019-06-10 10:25:00 +0800

    fix: Function.prototype._bind and add test

--- test69.js
+++ test69.js
@@ -1,11 +1,10 @@
 ;(function () {
 
   Function.prototype._bind = function () {
-    var context = arguments[0];
-    var args = arguments.slice(1);
+    var args = [].slice.call(arguments);
     var _this = this;
     return function fn () {
-      return _this.apply(context, args);
+      return _this.apply(args[0], args.slice(1));
     }
   }
 
@@ -19,8 +18,10 @@
     return this.a + this.b + this.c + a + b + c;
   }
 
-  var $fn = fn.bind(obj, 1, 2, 3);
+  var fn1 = fn.bind(obj, 1, 2, 3);
+  var fn2 = fn._bind(obj, 1, 2, 3);
 
-  console.log($fn());
+  console.log('fn1: ', fn1());
+  console.log('fn2: ', fn2());
 
 })();
