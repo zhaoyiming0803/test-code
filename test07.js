9a4a24bee571f5818bd1314ae06f76728c709e03
zhaoyiming <yiming_zhao@qingsongchou.com>
2019-08-23 14:45:07 +0800

    chore: adjust function name

--- test07.js
+++ test07.js
@@ -1,26 +1,26 @@
 ;(function () {
 
-	function getCurMonth() {
+	function getCurMonth () {
 		var date = new Date();
 		return date.getMonth() + 1;
 	}
 
 	// 某月共有几天
-	function getDays(curYeay, curMonth) {
+	function getDate (curYeay, curMonth) {
 		// 此处构造的日期为下个月的第0天，天数索引从1开始，第0天即代表上个月的最后一天
 		var date = new Date(curYeay, curMonth, 0);
 		return date.getDate();
 	};
 
 	// 本月第一天是周几
-	function getWeek(year, month) {
+	function getDay (year, month) {
 		var date = new Date(year, month - 1, 1);
 		return date.getDay();
 	}
 
 	var curMonth = getCurMonth();
 	console.log(curMonth);
-	console.log(getDays(2019, curMonth));
-	console.log(getWeek(2019, curMonth));
+	console.log(getDate(2019, curMonth));
+	console.log(getDay(2019, curMonth));
 
 })();
