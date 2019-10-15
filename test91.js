d16177fc17f678e5d1bc36330ccb60bca5a8184a
zymfe <1047832475@qq.com>
2019-10-15 13:13:40 +0800

    update promiseAll and promiseWaterfull

--- test91.js
+++ test91.js
@@ -40,9 +40,9 @@
     return next();
   }
 
-  // promiseWaterfull([p1, p2, p3]).then(res => {
-  //   console.log(res);
-  // });
+  promiseWaterfull([p1, p2, p3]).then(res => {
+    console.log(res);
+  });
 
 })();
 
@@ -89,66 +89,12 @@
     }
   }
 
-  // promiseAll([p1, p2, p3], res => {
-  //   console.log(res);
-  // });
+  promiseAll([p1, p2, p3], res => {
+    console.log(res);
+  });
 
 })();
 
 // promiseAll 比 promiseWaterfull 先输出结果
 // 因为 promiseAll 是并发执行
 // 而 promiseWaterfull 是在队列中按序执行
-
-;(function () {
-
-  function App () {
-    this.ctx = {
-      req: {},
-      res: {}
-    }
-    this.middleware = [];
-  }
-
-  App.prototype.use = function use (fn) {
-    this.middleware.push(fn);
-  }
-
-  App.prototype.run = function run () {
-    var middleware = this.middleware;
-    var ctx = this.ctx;
-
-    function dispatch (i) {
-      var fn = middleware[i];
-      if (!fn) return Promise.resolve();
-      return Promise.resolve(fn(ctx, function next () {
-        return dispatch(i + 1);
-      }));
-    }
-
-    return dispatch(0);
-  }
-
-  var app = new App();
-
-  app.use(async function m1 (ctx, next) {
-    console.log(ctx);
-    function query () {
-      return new Promise(function (resolve, reject) {
-        setTimeout(function () {
-          resolve('m1');
-        }, 1000);
-      });
-    }
-    const res = await query();
-    console.log(res);
-    await next(); // m2 是否执行取决于当前 next 方法是否执行
-    console.log('m1-1');
-  });
-
-  app.use(async function m2 (ctx, next) {
-    console.log('m2');
-  });
-
-  app.run();
-
-})();
