52a2ffed3f3e638a407790dea6ad2fa8a91cd34f
zymfe <1047832475@qq.com>
2019-10-30 17:49:57 +0800

    update

---
+++ test103.js
@@ -0,0 +1,17 @@
+// let arr = [];
+// while (true) {
+//   arr.push(1);
+// };
+
+
+// let arr = [];
+// while(true) {
+//   arr.push();
+//   console.log(arr.length); // 0
+// }
+
+// let arr = [];
+// while(true) {
+//   arr.push(Buffer.from(1000));
+//   console.log(arr.length);
+// }
