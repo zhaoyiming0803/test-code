db5302d6677c2ab8a19b19c297699bb3b736b626
zymfe <1047832475@qq.com>
2019-12-04 14:42:27 +0800

    test: type conversion

---
+++ test109.js
@@ -0,0 +1,59 @@
+;(function () {
+  // 为什么 [] == ![]
+  // https://segmentfault.com/a/1190000008594792
+  var a = [];
+  var b = ![];
+  console.log(a == b); // true
+  console.log(a == 0); // true
+  console.log(b == false); // true
+  console.log(a == false); // true
+  console.log([] == []); // false
+})();
+
+;(function () {
+  var a = 1;
+  var b = {};
+  console.log(a+b); // 1[object Object]
+
+  var c = 1;
+  var d = {
+    // 重写 valueOf 方法
+    valueOf: function () {
+      return 1;
+    }
+  };
+  console.log('c+d=' + (c + d));
+
+  var e = 1;
+  var f = {
+    // 重写 toString 方法
+    toString: function () {
+      return 2;
+    }
+  };
+  console.log('e+f=' + (e + f));
+})();
+
+;(function () {
+  var a = 1;
+  var b = {};
+  console.log(a+b); // 1[object Object]
+})();
+
+;(function () {
+  var a = 1;
+  var b = [];
+  console.log(a+b); // 1
+
+  var c = 1;
+  var d = [1];
+  console.log(c+d); // "11"
+
+  var e = 1;
+  var f = [1, 2, 3];
+  console.log(e+f); // "11,2, 3"
+
+  var g = 1;
+  var h = [1, 2, 3];
+  console.log(h+g); // "1, 2, 31"
+})();
