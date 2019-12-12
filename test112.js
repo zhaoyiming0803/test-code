4b80f838b11d1bb4738de0109ad71e71706b2852
zymfe <1047832475@qq.com>
2019-12-12 18:26:04 +0800

    test: generator

--- test112.js
+++ test112.js
@@ -26,17 +26,24 @@
 
 ;(function () {
 
+  // 需要自己封装工具方法
+  // 完成一个请求之后，手动调用 next 方法，执行下一个步骤
+  // https://github.com/Jocs/jocs.github.io/issues/11
   function request (val) {
     setTimeout(() => {
-      console.log(val);
-      genRes.next();
-    }, 50);
+      genRes.next(val);
+    }, 1000);
   }
 
   function* gen () {
-    yield request(1);
-    yield request(2);
-    yield request(3);
+    // yield 表达式本身没有返回值，或者说总是返回 undefined。
+    // next 方法可以带一个参数，该参数就会被当作上一个 yield 表达式的返回值。
+    const val1 = yield request(1111);
+    console.log(val1);
+    const val2 = yield request(2222);
+    console.log(val2);
+    const val3 = yield request(3333);
+    console.log(val3);
   }
 
   const genRes = gen();
