5b9e6708701063a73899b2ad441be07537a46c4c
zymfe <1047832475@qq.com>
2019-12-21 23:33:13 +0800

    fix:  find addition numbers

--- test99.js
+++ test99.js
@@ -1,21 +1,18 @@
 ;(function () {
 
-  var arr = [1, 5, 3, 9, 4, 0, 2];
+  var arr = [1, 2, 3, 9, 4, 0, 5];
 
   var sum = 3;
 
   function findAdditionNumbers (arr, sum) {
     var result = [];
-
     var middle = Math.floor(arr.length / 2);
-
     for (var i = 0; i < middle; i++) {
       var index = arr.indexOf(sum - arr[i]);
-      if (index >= 0) {
+      if (index >= 0 && result.toString().split(',').indexOf(arr[index]+'') === -1) {
         result.push([arr[i], arr[index]]);
       }
     }
-
     return result;
   }
 
