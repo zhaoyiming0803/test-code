8d7a884ba0cf9028b36aa66c4b71599eb721e9cf
zymfe <1047832475@qq.com>
2020-01-22 09:08:18 +0800

    update: search nums

--- test133.js
+++ test133.js
@@ -1,37 +1,33 @@
 ; (function () {
 
   function search (sum, arr) {
-    function _search (sum, arr) {
-      if (arr.length < 3) {
-        return
-      }
+    if (arr.length < 3) {
+      return
+    }
 
-      var res = []
-      var startIdx = 0
-      var endIdx = arr.length - 1
+    var res = []
+    var startIdx = 0
+    var endIdx = arr.length - 1
 
-      while (startIdx <= endIdx && startIdx !== endIdx) {
-        if (arr[ startIdx ] >= sum) {
-          startIdx += 1
-        } else if (arr[ endIdx ] >= sum) {
-          endIdx -= 1
-        } else if (arr[ startIdx ] + arr[ endIdx ] >= sum) {
-          return search(sum, arr.slice(startIdx, endIdx)) || search(sum, arr.slice(startIdx + 1, endIdx + 1))
-        } else {
-          var diff = sum - arr[ startIdx ] - arr[ endIdx ]
-          var idx = arr.findIndex(item => item === diff)
-          if (idx !== -1) {
-            return [ arr[ startIdx ], arr[ endIdx ], arr[ idx ] ]
-          }
+    while (startIdx <= endIdx && startIdx !== endIdx) {
+      if (arr[ startIdx ] >= sum) {
+        startIdx += 1
+      } else if (arr[ endIdx ] >= sum) {
+        endIdx -= 1
+      } else if (arr[ startIdx ] + arr[ endIdx ] >= sum) {
+        return search(sum, arr.slice(startIdx, endIdx)) || search(sum, arr.slice(startIdx + 1, endIdx + 1))
+      } else {
+        var diff = sum - arr[ startIdx ] - arr[ endIdx ]
+        var idx = arr.findIndex(item => item === diff)
+        if (idx !== -1) {
+          return [ arr[ startIdx ], arr[ endIdx ], arr[ idx ] ]
         }
       }
     }
-
-    return _search(sum, arr)
   }
 
   console.time('searchTime')
-  console.log(search(22, [ 10, 20, 30, 40, 50, 60, 70, 1, 2, 76, 3, 5, 9, 24, 6, 4, 8, 7, 12, 12, 23, 13, 34, 56, 67, 34, 34, 45, 223, 123, 23, 32, 34, 45 ]))
+  console.log(search(21, [ 10, 20, 30, 40, 50, 60, 70, 1, 2, 76, 3, 5, 9, 24, 6, 4, 8, 7, 12, 12, 23, 13, 34, 56, 67, 34, 34, 45, 223, 123, 23, 32, 34, 45 ]))
   console.timeEnd('searchTime')
 
 })();
