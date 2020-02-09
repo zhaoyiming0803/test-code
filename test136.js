b04743bffd58b777c62b195df0c216fb8f6b8af4
zymfe <1047832475@qq.com>
2020-02-09 17:27:30 +0800

    test: create circle link and insert a randomly insert a node

---
+++ test136.js
@@ -0,0 +1,56 @@
+; (function () {
+
+  var firstNode = null
+  var num = 1
+
+  function createNewNode (num) {
+    return {
+      expirationTime: num,
+      next: null,
+      previous: null
+    }
+  }
+
+  function createLink () {
+    firstNode = createNewNode(num)
+    var node = firstNode
+    while (node.next === null) {
+      if (num >= 10) {
+        node.next = firstNode
+        firstNode.previous = node
+        break
+      }
+      num += 1
+      var newNode = createNewNode(num)
+      newNode.previous = node
+      node.next = newNode
+      node = node.next
+    }
+  }
+
+  createLink()
+
+  console.log(firstNode)
+
+  function insertNode () {
+    var random = Math.random() * 10
+    var newNode = createNewNode(random)
+    var node = firstNode
+    var next = null
+    do {
+      if (node.expirationTime > random) {
+        next = node
+        break
+      }
+      node = node.next
+    } while (node !== firstNode)
+    var previous = next.previous
+    previous.next = next.previous = newNode;
+    newNode.previous = previous
+    newNode.next = next
+  }
+
+  insertNode()
+
+  console.log(firstNode)
+})();
