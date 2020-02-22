90c2419e3860b011de75cd87654cb265cbef8c6b
zymfe <1047832475@qq.com>
2020-02-22 16:06:35 +0800

    test module, add comments

--- test140/b.js
+++ test140/b.js
@@ -1,4 +1,5 @@
 let { person, count } = require('./a')
 person.age = 19
 count += 1
+// { person: { name: 'zhaoyiming', age: 19 }, count: 0 }
 console.log(require('./a'))
---
+++ test140/c.js
@@ -0,0 +1,2 @@
+// { person: { name: 'zhaoyiming', age: 18 }, count: 0 }
+console.log(require('./a'))
