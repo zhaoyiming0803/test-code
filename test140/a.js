2b5e7a0d8f068b22967046116074996b207439ae
zymfe <1047832475@qq.com>
2020-02-22 15:59:38 +0800

    test module

---
+++ test140/a.js
@@ -0,0 +1,6 @@
+exports.person = {
+  name: 'zhaoyiming',
+  age: 18
+}
+
+exports.count = 0
---
+++ test140/b.js
@@ -0,0 +1,4 @@
+let { person, count } = require('./a')
+person.age = 19
+count += 1
+console.log(require('./a'))
