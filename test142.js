06910feb90bcbe897ce2864ce925306bf2181890
zymfe <1047832475@qq.com>
2020-02-29 17:42:35 +0800

    luck draw

---
+++ test142.js
@@ -0,0 +1,16 @@
+;(function () {
+
+    var list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n']
+
+    console.log('start: ', list.length)
+
+    for (var i = 0; i < 5; i++) {
+      var random = Math.floor(Math.random() * list.length)
+      console.log(list[random])
+      list[random] = list[list.length - 1]
+      list.length--
+    }
+
+    console.log('end: ', list.length)
+
+})()
