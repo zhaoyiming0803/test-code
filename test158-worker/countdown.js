e9ee974ffe6dd77960173b84e0fa2a87dfad37f1
zymfe <1047832475@qq.com>
2020-03-18 14:33:21 +0800

    update: countdown by Worker

--- test158-worker/countdown.js
+++ test158-worker/countdown.js
@@ -1,11 +1,24 @@
 self.onmessage = event => {
-  var num = event.data
-  var timer = setInterval(() => {
-    if (num < 1) {
+  let timeArr = event.data
+
+  let timer = setInterval(() => {
+    let isComplete = true
+
+    timeArr = timeArr.map(num => {
+      num -= 1
+      if (num < 0) {
+        return 0
+      }
+      isComplete = false
+      return num
+    })
+
+    if (isComplete) {
+      console.log('completed')
       clearInterval(timer)
       self.close()
     } else {
-      self.postMessage(--num)
+      self.postMessage(timeArr)
     }
   }, 1000)  
 }
