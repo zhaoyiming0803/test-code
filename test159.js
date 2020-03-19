504d7b029c9c236f7b0dd8cee81f96898915f638
zymfe <1047832475@qq.com>
2020-03-20 00:23:33 +0800

    update function name, reset priority

--- test159.js
+++ test159.js
@@ -2,11 +2,11 @@
 
   const arr = [ 0, 1, 2, 3, 4, 5, 6, 7 ]
 
-  function handle (arr) {
+  function resetPriority (arr) {
     const index = Math.floor(Math.random() * arr.length)
     return [ arr[ index ] ].concat(arr.slice(0, index), arr.slice(index + 1))
   }
 
-  console.log(handle(arr))
+  console.log(resetPriority(arr))
 
 })();
