ac8966500b8b0fc6ae5b3461adafca40568177dc
zymfe <1047832475@qq.com>
2020-03-20 00:20:20 +0800

    test: reset priority

---
+++ test159.js
@@ -0,0 +1,12 @@
+; (function () {
+
+  const arr = [ 0, 1, 2, 3, 4, 5, 6, 7 ]
+
+  function handle (arr) {
+    const index = Math.floor(Math.random() * arr.length)
+    return [ arr[ index ] ].concat(arr.slice(0, index), arr.slice(index + 1))
+  }
+
+  console.log(handle(arr))
+
+})();
