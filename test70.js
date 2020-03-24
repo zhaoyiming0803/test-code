2c2d5086edd49e181dc8bbc66d4db87805e30744
zymfe <1047832475@qq.com>
2020-03-25 00:38:19 +0800

    update: new Function

--- test70.js
+++ test70.js
@@ -2,11 +2,32 @@
 // 当运行这些函数的时候，它们只能访问自己的本地变量和全局变量，不能访问Function构造器被调用生成的上下文的作用域。
 // 这和使用带有函数表达式代码的 eval 不同
 
-var n = 1;
+;(function () {
 
-function fn () {
-  var n = 2;
-  return new Function('a', 'b', 'return a + b + n;');
-}
+  var n = 1;
 
-console.log(fn()(1, 2));
+  function fn () {
+    var n = 2;
+    // return new Function('a', 'b', 'return a + b');
+    return new Function('a', 'b', 'return a + b + n;');
+  }
+
+  console.log(fn()(1, 2));
+
+})();
+
+;(function () {
+
+  var str = 'var a = 1; var b = 2; return a + b + this.c;'
+
+  var obj = {
+    c: 100
+  }
+
+  var fn = new Function(str).bind(obj)
+
+  console.log(fn())
+
+  eval('var a = 1; var b = 2; console.log(a+b)')
+
+})();
