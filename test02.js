34de4bf152345c436f0fb76bedc398ba9e0e43ec
zymfe <1047832475@qq.com>
2020-04-06 19:29:11 +0800

    test: singleton mode by closure

--- test02.js
+++ test02.js
@@ -14,3 +14,29 @@
   console.log(innerFn()); // 5
   console.log(innerFn()); // 6
 })();
+
+(function () {
+  const Person = (function () {
+    let instance = null;
+
+    function PersonClass(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+
+    return function (name, age) {
+      if (!instance) {
+        instance = new PersonClass(name, age);
+      }
+      return instance;
+    };
+  })();
+
+  const p1 = new Person("zhangsan", 19);
+  const p2 = new Person("lisi", 18);
+  const p3 = new Person("wangwu", 20);
+
+  console.log(p1.name); // zhangsan
+  console.log(p2.name); // zhangsan
+  console.log(p3.name); // zhangsan
+})();
