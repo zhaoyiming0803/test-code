7a471330cb3a1d01cb36f280dd9ae7a1a7427909
zymfe <1047832475@qq.com>
2020-04-06 22:00:11 +0800

    update: Math.max and Math.min

--- test08.js
+++ test08.js
@@ -1,38 +1,36 @@
-;(function () {
+(function () {
+  var arr = [1, 100, -99, 87, 101, 9];
 
-	var arr = [1, 100, -99, 87, 101, 9];
+  function getMaxVal(arr) {
+    return Math.max.apply(Math, arr);
+  }
 
-	function getMaxVal (arr) {
-		return Math.max.apply(Math, arr);
-	}
+  function getMinVal(arr) {
+    return Math.min.apply(Math, arr);
+  }
 
-	function getMinVal (arr) {
-		return Math.min.apply(Math, arr);
-	}
+  function getMaxIdx(arr) {
+    var maxIdx = 0;
+    for (var i = 1; i < arr.length; i += 1) {
+      if (arr[maxIdx] < arr[i]) {
+        maxIdx = i;
+      }
+    }
+    return maxIdx;
+  }
 
-	function getMaxIdx (arr) {
-		var maxIdx = 0;
-		for (var i = 1; i < arr.length; i += 1) {
-			if (arr[maxIdx] < arr[i]) {
-				maxIdx = i;
-			}
-		}
-		return maxIdx;
-	}
+  function getMinIdx(arr) {
+    var minIdx = 0;
+    for (var i = 1; i < arr.length; i += 1) {
+      if (arr[minIdx] > arr[i]) {
+        minIdx = i;
+      }
+    }
+    return minIdx;
+  }
 
-	function getMinIdx (arr) {
-		var minIdx = 0;
-		for (var i = 1; i < arr.length; i += 1) {
-			if (arr[minIdx] > arr[i]) {
-				minIdx = i;
-			}
-		}
-		return minIdx;
-	}
-
-	console.log(getMaxVal(arr));
-	console.log(getMinVal(arr));
-	console.log(getMaxIdx(arr));
-	console.log(getMinIdx(arr));
-
+  console.log(getMaxVal(arr));
+  console.log(getMinVal(arr));
+  console.log(getMaxIdx(arr));
+  console.log(getMinIdx(arr));
 })();
