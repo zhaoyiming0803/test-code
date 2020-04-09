6e2ee85ed26c736223095e0a1f7a63660d6bd344
zymfe <1047832475@qq.com>
2020-04-09 22:05:34 +0800

    update: co function for auto exec generate function

--- test126.js
+++ test126.js
@@ -1,26 +1,46 @@
-; (function () {
-
+(function () {
   // reference test137.js
 
   function co(gen) {
     const g = gen();
-    next();
-    function next(value) {
-      const res = g.next(value);
-      if (!res.done) {
-        if (Object.prototype.toString.call(res.value) === '[object Promise]') {
-          res.value.then(res => next(res))
-        } else {
-          next(res.value)
-        }
+
+    return new Promise(function (resolve, reject) {
+      if (!g || typeof g.next !== "function") {
+        return reject(g);
       }
-    }
+
+      next();
+
+      function next(value) {
+        const res = g.next(value);
+
+        if (res.done) {
+          return resolve(res.value);
+        }
+
+        if (Object.prototype.toString.call(res.value) === "[object Promise]") {
+          return res.value.then(
+            (res) => next(res),
+            (error) => reject(error)
+          );
+        }
+
+        return reject(
+          new TypeError(
+            "You may only yield a function, promise, generator, array, or object, " +
+              'but the following object was passed: "' +
+              String(res.value) +
+              '"'
+          )
+        );
+      }
+    });
   }
 
   function* fn() {
     const a = yield p1();
     const b = yield p2();
-    console.log(a + b);
+    return a + b;
   }
 
   function p1() {
@@ -31,14 +51,27 @@
     });
   }
 
+  // function p2() {
+  //   return new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       resolve(2);
+  //     }, 1000);
+  //   });
+  // }
+
   function p2() {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        resolve(2);
+        reject("cry...");
       }, 1000);
     });
   }
 
-  co(fn);
-
+  co(fn)
+    .then((res) => {
+      console.log("res", res);
+    })
+    .catch((res) => {
+      console.log("error: ", res);
+    });
 })();
