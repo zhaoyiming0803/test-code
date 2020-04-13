0056cbe566567f18774f8f0d99a9c3d25d271bcd
zymfe <1047832475@qq.com>
2020-04-13 19:24:09 +0800

    udpate: randomSort

--- test161.js
+++ test161.js
@@ -2,41 +2,37 @@
  * 洗牌算法，连续运行10次，第二种比第一种更加均匀
  */
 
-;(function () {
+(function () {
+  return;
 
-  return
-
-  function randomSort () {
-    var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  function randomSort() {
+    var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     for (var i = 0; i < arr.length; i++) {
-      arr.sort((a, b) => Math.random() > 0.5)
-      console.log(arr)
+      arr.sort((a, b) => Math.random() > 0.5);
+      console.log(arr);
     }
   }
 
-  randomSort()
-
+  randomSort();
 })();
 
-;(function () {
+(function () {
+  function randomSort() {
+    var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var len = arr.length;
 
-  function randomSort () {
-    var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    var len = arr.length
-    
-    for (var j = 0; j < len; j++) {
-      for (var i = 0; i < len; i++) {
-        var random = Math.floor(Math.random() * len)
-        // arr[random] = arr.splice(i, 1, arr[random])[0]
-        var a = arr[random]
-        var b = arr[i]
-        arr[random] = b
-        arr[i] = a
-      }
-      console.log(arr)
+    // for (var j = 0; j < len; j++) {
+    for (var i = 0; i < len; i++) {
+      var random = Math.floor(Math.random() * len);
+      // arr[random] = arr.splice(i, 1, arr[random])[0]
+      var a = arr[random];
+      var b = arr[i];
+      arr[random] = b;
+      arr[i] = a;
     }
+    console.log(arr);
+    // }
   }
 
-  randomSort()
-
+  randomSort();
 })();
