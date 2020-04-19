843de12c5b956eb2af9212c4d344cc4a6e414470
zymfe <1047832475@qq.com>
2020-04-19 21:36:21 +0800

    update: slice function

--- test187.js
+++ test187.js
@@ -7,3 +7,22 @@
   console.log(arr1); //  0, 1, 2, 3, 4, { a: 200 } ]
   console.log(arr2); // [ 100, 1, 2, 3, 4, { a: 200 } ]
 })();
+
+(function () {
+  // 如果 slice 方法的参数中有负数，则用数组长度加该数来确定相应的位置
+  // 如果结束位置小于起始位置，则返回空数组
+  var arr1 = [0, 1, 2, 3, 4, 5, 6];
+
+  var arr2 = arr1.slice(2, -2);
+  var arr3 = arr1.slice(2, -2 + arr1.length);
+  console.log(arr2); // [ 2, 3, 4 ]
+  console.log(arr3); // [ 2, 3, 4 ]
+
+  var arr4 = arr1.slice(-4, -2);
+  var arr5 = arr1.slice(-4 + arr1.length, -2 + arr1.length);
+  console.log(arr4); // [ 3, 4 ]
+  console.log(arr5); // [ 3, 4 ]
+
+  var arr6 = arr1.slice(-1, -4);
+  console.log(arr6); // []
+})();
