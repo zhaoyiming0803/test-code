ee5100614377a2d59ceaa5676f025bcd0a2626f7
zymfe <1047832475@qq.com>
2020-04-19 22:45:45 +0800

    update: RegExp match exec

--- test162.js
+++ test162.js
@@ -1,5 +1,4 @@
-;(function () {
-
+(function () {
   /**
    * 如果 regexp 没有标志 g，那么 match() 方法就只能在 stringObject 中执行一次匹配。
    * 如果没有找到任何匹配的文本， match() 将返回 null。
@@ -8,18 +7,18 @@
    * 除了这些常规的数组元素之外，返回的数组还含有两个对象属性。
    * index 属性声明的是匹配文本的起始字符在 stringObject 中的位置，
    * input 属性声明的是对 stringObject 的引用。
-   * 
-   * 
+   *
+   *
    * 如果 regexp 具有标志 g，则 match() 方法将执行全局检索，
    * 找到 stringObject 中的所有匹配子字符串。
    * 若没有找到任何匹配的子串，则返回 null。
    * 如果找到了一个或多个匹配子串，则返回一个数组。
    * 不过全局匹配返回的数组的内容与前者大不相同，它的数组元素中存放的是 stringObject 中所有的匹配子串，
    * 而且也没有 index 属性或 input 属性。
-   * 
-   * 注意：在全局检索模式下，match() 即不提供与子表达式匹配的文本的信息，也不声明每个匹配子串的位置。
+   *
+   * 注意：在全局检索模式下，match() 既不提供与子表达式匹配的文本的信息，也不声明每个匹配子串的位置。
    * 如果您需要这些全局检索的信息，可以使用 RegExp.exec()。
-   * 
+   *
    * 当 RegExpObject 是一个全局正则表达式时，exec() 的行为就稍微复杂一些。
    * 它会在 RegExpObject 的 lastIndex 属性指定的字符处开始检索字符串 string。
    * 当 exec() 找到了与表达式相匹配的文本时，在匹配后，
@@ -28,21 +27,46 @@
    * 当 exec() 再也找不到匹配的文本时，它将返回 null，并把 lastIndex 属性重置为 0。
    */
 
-  var str = 'hello123Aworld456Bjavascript'
+  var str = "hello123Aworld456Bjavascript";
 
-  var matchRes = str.match(/[0-9]+([A-Z])/g)
-  console.log(matchRes)
+  var matchRes = str.match(/[0-9]+([A-Z])/g);
+  console.log(matchRes);
 
-  console.log('------------')
+  console.log("------------");
 
-  var execRes = null
+  var execRes = null;
   // 这个地方一定要把正则定义成变量
   // 直接写在 while 里，会导致死循环
-  var reg = /[0-9]+([A-Z])/g
-  while (execRes = reg.exec(str)) {
-    console.log(execRes)
-    console.log(reg.lastIndex)
+  // 下面的代码，如果把正则写进 while 里，则相当于每次调用新的正则实例，所以返回的结果都一样，
+  // while 内条件始终为 true，最终会死循环
+  var reg = /[0-9]+([A-Z])/g;
+  while ((execRes = reg.exec(str))) {
+    console.log(execRes);
+    console.log(reg.lastIndex);
   }
-  console.log(reg.lastIndex)
+  console.log(reg.lastIndex);
+})();
 
+(function () {
+  // JavaScript高级程序设计-104页 底部 105页上半部分
+  // 在 ECMAScript3 中，正则表达式字面量始终会同享同一个 RegExp 实例
+  // 使用构造函数创建的 RegExp 实例每次都是一个新实例。
+  // ECMAScript5 规定：使用正则表达式字面量，必须像直接调用 RegExp 构造函数一样，每次都创建新的实例
+
+  var str = "hello123Aworld456Bjavascript";
+  var reg = null;
+
+  for (var i = 0; i < 10; i++) {
+    reg = /[0-9]+([A-Z])/g;
+    console.log(reg.test(str)); // true true false true true false true true false true
+  }
+
+  console.log("-----------");
+
+  // 如果将 for 循环体内的 reg 放到循环体外，那么也和上面的字面量结果是一样的
+  // reg = new RegExp("[0-9]+([A-Z])", "g");
+  for (var i = 0; i < 10; i++) {
+    reg = new RegExp("[0-9]+([A-Z])", "g");
+    console.log(reg.test(str)); // true true true true true true true true true true
+  }
 })();
