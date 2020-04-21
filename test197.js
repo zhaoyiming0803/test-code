461fdbed68d42c378930067b03a8109e9d47d14b
zymfe <1047832475@qq.com>
2020-04-21 13:55:57 +0800

    函数作用域是在函数定义时确定的,而不是在函数执行时

---
+++ test197.js
@@ -0,0 +1,16 @@
+(function () {
+  // 证明作用域是在函数定义时确定的，而不是在函数执行时
+  // 如果是作用域在函数执行时确定，那么 getStr() 是可以访问到 defaultStr 变量的
+  // 解决方法：把 defaultStr 变量放到和 fn、getStr 同一层即可。
+  function fn() {
+    var defaultStr = "hello world";
+    var str = getStr();
+    return "get [" + str + "] from " + arguments.callee.name;
+  }
+
+  function getStr() {
+    return defaultStr; // defaultStr is not defined
+  }
+
+  console.log(fn());
+})();
