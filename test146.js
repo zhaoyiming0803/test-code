ef1d7676bc1247ae15bb51ab7891420c6ea3fac6
zymfe <1047832475@qq.com>
2020-04-29 16:39:27 +0800

    update: mock vue router data struct

--- test146.js
+++ test146.js
@@ -1,62 +1,61 @@
-; (function () {
-
+(function () {
   // mock vue router data struct
 
   const routes = [
     {
-      path: '/',
-      component: 'App.page'
+      path: "/",
+      component: "App.page",
     },
     {
-      path: '/home',
-      component: 'Home.page',
+      path: "/home",
+      component: "Home.page",
       children: [
-        { 
-          path: 'top', 
-          component: 'HomeTop.page',
+        {
+          path: "top",
+          component: "HomeTop.page",
           children: [
             {
-              path: '/top-center',
-              component: 'HomeTopCenter.page'
-            }
-          ]
-        }
-      ]
+              path: "/top-center",
+              component: "HomeTopCenter.page",
+            },
+          ],
+        },
+      ],
     },
     {
-      path: '/list',
-      component: 'List.page'
-    }
-  ]
+      path: "/list",
+      component: "List.page",
+    },
+  ];
 
-  function normalizePath (path) {
-    return path.replace(/\/$/, '')
+  function normalizePath(path) {
+    return path.replace(/\/$/, "");
   }
 
-  function normalizeChildPath (path) {
-    if (path.charAt(0) === '/') {
-      return path.slice(1)
+  function normalizeChildPath(path) {
+    // 如果不是一级目录，就把第一个字符 / 去掉
+    if (path.charAt(0) === "/") {
+      return path.slice(1);
     }
-    return path
+    return path;
   }
 
-  function createMatcher (routes, matcher = {}, parent) {
-    routes.forEach(route => {
+  function createMatcher(routes, matcher = {}, parent) {
+    routes.forEach((route) => {
       const record = {
         path: normalizePath(route.path),
-        component: route.component
-      }
+        component: route.component,
+      };
       if (parent) {
-        record.path = `${parent.path}/${normalizeChildPath(route.path)}`
+        record.path = `${parent.path}/${normalizeChildPath(route.path)}`;
       }
-      matcher[record.path] = record
+      matcher[record.path] = record;
       if (route.children) {
-        return createMatcher(route.children, matcher, record)
+        return createMatcher(route.children, matcher, record);
       }
-    })
-    return matcher
+    });
+    return matcher;
   }
 
-  console.log(createMatcher(routes))
-
+  console.log(createMatcher(routes));
 })();
