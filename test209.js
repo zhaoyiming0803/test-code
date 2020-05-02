88e11d1534fcdf2ccdfbf83a75de21f38d4b99e3
zymfe <1047832475@qq.com>
2020-05-02 22:52:27 +0800

    test: getMaxSuccessiveLength

--- test209.js
+++ test209.js
@@ -1,7 +1,49 @@
 !(function () {
-  var arr = [20, 5, 9, 100, 4, 6, 8, 2, 1, 3];
+  function getMaxSuccessiveLength(arr) {
+    const arrSet = new Set(arr);
+    let res = [];
+    let temp = [];
 
-  function getMaxLength(arr) {}
+    for (let i = 0; i < arr.length; i++) {
+      const v = arr[i];
+      const hasPre = arrSet.has(v - 1);
+      const hasNext = arrSet.has(v + 1);
 
-  console.log(getMaxLength(arr));
+      if (!hasPre && !hasNext) {
+        continue;
+      }
+
+      if (hasPre) {
+        let i = v - 1;
+        temp.push(i, v);
+        while (arrSet.has(--i)) {
+          temp.unshift(i);
+        }
+      }
+
+      if (hasNext) {
+        let i = v + 1;
+        if (hasPre) {
+          temp.push(i);
+        } else {
+          temp.push(v, i);
+        }
+        while (arrSet.has(++i)) {
+          temp.push(i);
+        }
+      }
+
+      if (temp.length > res.length) {
+        res = temp;
+      }
+
+      temp = [];
+    }
+
+    return res;
+  }
+
+  console.log(getMaxSuccessiveLength([20, 5, 8, 7, 9, 100, 2, 1, 3, 6, 10]));
+  console.log(getMaxSuccessiveLength([4, 5, 3, 1, 2]));
+  console.log(getMaxSuccessiveLength([8, 4, 5, 7, 3, 11, 13, 1, 2, 9, 6]));
 })();
