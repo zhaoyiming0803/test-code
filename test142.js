8e1cae764cfedf6ad107b483e089bc47be0e2818
zymfe <1047832475@qq.com>
2020-05-07 22:02:22 +0800

    update: luck draw

--- test142.js
+++ test142.js
@@ -1,16 +1,30 @@
-;(function () {
+(function () {
+  // 随机营运抽奖或洗牌
+  var list = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+  ];
 
-    var list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n']
+  console.log("start: ", list.length);
 
-    console.log('start: ', list.length)
+  for (var i = 0; i < 5; i++) {
+    var random = Math.floor(Math.random() * list.length);
+    console.log(list[random]);
+    list[random] = list[list.length - 1];
+    list.length--;
+  }
 
-    for (var i = 0; i < 5; i++) {
-      var random = Math.floor(Math.random() * list.length)
-      console.log(list[random])
-      list[random] = list[list.length - 1]
-      list.length--
-    }
-
-    console.log('end: ', list.length)
-
-})()
+  console.log("end: ", list.length);
+})();
