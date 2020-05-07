f25b36a2bb75355a54ae2cfeac1e386e671b6d1b
zymfe <1047832475@qq.com>
2020-05-07 22:16:37 +0800

    update: new Function, mock template engine and compiler engine

--- test210.js
+++ test210.js
@@ -1,7 +1,34 @@
-~(function () {
+//github.com/zymfe/test-code/blob/master/test70.js
+https: ~(function () {
   function evil(str) {
     return new Function("return " + str);
   }
-  console.log(evil("1+2*5")());
-  console.log(evil("3/2*5")());
+  console.log(evil("1+2*5")()); // 11
+  console.log(evil("3/2*5")()); // 7.5
+})();
+
+~(function () {
+  function fn() {
+    return new Function("c", "var a = 1; var b = 2;return a + b + c;");
+  }
+  console.log(fn()(100)); // 103
+})();
+
+~(function () {
+  // 注意这里与第一个demo的区别
+  function evil() {
+    return new Function("str", "return str;");
+  }
+  console.log(evil()("1+2*5")); // 1+2*5
+  console.log(evil()("3/2*5")); // 3/2*5
+})();
+
+~(function () {
+  // 注意这里与第一个demo的区别
+  function evil(str) {
+    // str is not defined
+    // 只能访问自己的本地变量和全局变量，不能访问Function构造器被调用生成的上下文的作用域
+    return new Function("return str;");
+  }
+  console.log(evil()("hello"));
 })();
--- test70.js
+++ test70.js
@@ -1,74 +1,72 @@
+//github.com/zymfe/test-code/blob/master/test210.js
 // 使用Function构造器生成的函数，并不会在创建它们的上下文中创建闭包；它们一般在全局作用域中被创建。
 // 当运行这些函数的时候，它们只能访问自己的本地变量和全局变量，不能访问Function构造器被调用生成的上下文的作用域。
 // 这和使用带有函数表达式代码的 eval 不同
 
-;(function () {
-
-  return
+(function () {
+  return;
 
   var n = 1;
 
-  function fn () {
+  function fn() {
     var n = 2;
     // return new Function('a', 'b', 'return a + b');
-    return new Function('a', 'b', 'return a + b + n;');
+    return new Function("a", "b", "return a + b + n;");
   }
 
   console.log(fn()(1, 2));
-
 })();
 
-;(function () {
-
-  var str = 'var a = 1; var b = 2; return a + b + this.c;'
+(function () {
+  var str = "var a = 1; var b = 2; return a + b + this.c;";
 
   var obj = {
-    c: 100
-  }
+    c: 100,
+  };
 
-  var fn = new Function(str).bind(obj)
+  var fn = new Function(str).bind(obj);
 
-  console.log(fn())
+  console.log(fn());
 
-  eval('var a = 1; var b = 2; console.log(a+b)')
-
+  eval("var a = 1; var b = 2; console.log(a+b)");
 })();
 
-;(function () {
+(function () {
+  var str =
+    "var r = []; for (var i = 0; i < data.length; i++) {r.push(data[i])} return r;";
 
-  var str = 'var r = []; for (var i = 0; i < data.length; i++) {r.push(data[i])} return r;'
-  
-  var data = [{
-    id: 1,
-    name: 'zhangsan'
-  }, {
-    id: 2,
-    name: 'lisi'
-  }, {
-    id: 3,
-    name: 'wangwu'
-  }]
-  var fn = new Function('data', str)
-  var res = fn(data)
-  console.log(res)
-  console.log(Object.prototype.toString.call(res))
-
+  var data = [
+    {
+      id: 1,
+      name: "zhangsan",
+    },
+    {
+      id: 2,
+      name: "lisi",
+    },
+    {
+      id: 3,
+      name: "wangwu",
+    },
+  ];
+  var fn = new Function("data", str);
+  var res = fn(data);
+  console.log(res);
+  console.log(Object.prototype.toString.call(res));
 })();
 
-;(function () {
-
+(function () {
   // https://github.com/zymfe/ztpl
 
-  var str = 'var r = [];'
-  str += 'for (var i = 0; i < 3; i++) { '
-  str += 'r.push("<div>索引：");'
-  str += 'r.push(i);'
-  str += 'r.push("</div>");'
-  str += '};'
-  str += 'r.push("<p>hello template engine</p>");'
-  str += 'return r.join("")'
+  var str = "var r = [];";
+  str += "for (var i = 0; i < 3; i++) { ";
+  str += 'r.push("<div>索引：");';
+  str += "r.push(i);";
+  str += 'r.push("</div>");';
+  str += "};";
+  str += 'r.push("<p>hello template engine</p>");';
+  str += 'return r.join("")';
 
-  var fn = new Function(str)
-  console.log(fn())
-
+  var fn = new Function(str);
+  console.log(fn());
 })();
