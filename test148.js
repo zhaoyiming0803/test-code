4c84e8becef1dbb4ffef8edc564114fb12fad7f1
zymfe <1047832475@qq.com>
2020-05-07 21:58:25 +0800

    update: binary search

--- test148.js
+++ test148.js
@@ -1,6 +1,7 @@
-;(function () {
-
-  var arr = [1, 2, 3, 100, 4, 8, 101, 54, 93, 27, 65]
+(function () {
+  // binary search
+  // 二分查找的数组必须是有序列表
+  var arr = [1, 2, 3, 100, 4, 8, 101, 54, 93, 27, 65];
 
   // function search (arr, target) {
   //   if (arr.length <= 1) {
@@ -17,23 +18,26 @@
   //   }
   // }
 
-  function search (arr, target) {
-    let middleIndex
-    while (middleIndex = Math.ceil(arr.length / 2)) {
-      if (arr.length <= 1) {
-        if (target === arr[0]) return target
-        return
+  function search(arr, target) {
+    let middleIndex;
+    while ((middleIndex = Math.ceil(arr.length / 2))) {
+      if (arr.length === 0) {
+        return;
       }
       if (target > arr[middleIndex]) {
-        arr = arr.slice(middleIndex)
+        arr = arr.slice(middleIndex);
       } else if (target < arr[middleIndex]) {
-        arr = arr.slice(0, middleIndex)
+        arr = arr.slice(0, middleIndex);
       } else {
-        return target
+        return target;
       }
     }
   }
 
-  console.log(search(arr.sort((a, b) => a - b), 8))
-
+  console.log(
+    search(
+      arr.sort((a, b) => a - b),
+      8
+    )
+  );
 })();
