0e3ffc789cce2af0a9465da4c41a86d08ec34e84
zymfe <1047832475@qq.com>
2020-05-11 22:38:10 +0800

    test: Array.prototype._reduce Array.prototype._reduceRight

--- test125.js
+++ test125.js
@@ -1,6 +1,6 @@
-;(function () {
-
-  function compose () {
+(function () {
+  // https://github.com/zymfe/test-code/blob/master/test35.js
+  function compose() {
     var args = [].slice.call(arguments);
     // return function (arg) {
     //   return args[2](args[1](args[0](arg)));
@@ -9,48 +9,50 @@
       return args.reduce((arg, next) => {
         return next(arg);
       }, arg);
-    }
+    };
   }
 
-  Array.prototype._reduceRight = function (fn, val) {
-    const len = this.length
+  Array.prototype._reduceRight = function (cb, base) {
+    const len = this.length;
 
     if (!len) {
-      return this
+      return this;
     }
 
-    let i = len - 1
-    let _val = val || this[i--]
+    let i = len - 1;
+    let res = base || this[i];
 
-    while (i >= 0) {
-      _val = fn(_val, this[i--])
+    while (i > 0) {
+      res = cb(res, this[--i]);
     }
 
-    return _val
-  }
+    return res;
+  };
 
-  function composeRight () {
-    var args = [].slice.call(arguments)
+  function composeRight() {
+    var args = [].slice.call(arguments);
     return function (arg) {
       return args._reduceRight((arg, next) => {
-        return next(arg)
-      }, arg)
-    }
+        return next(arg);
+      }, arg);
+    };
   }
 
-  function reverse (str) {
-    return str.split('').reverse().join('');
+  function reverse(str) {
+    return str.split("").reverse().join("");
   }
 
-  function toUpperCase (str) {
+  function toUpperCase(str) {
     return str.toUpperCase();
   }
 
-  function toLowerCase (str) {
+  function toLowerCase(str) {
     return str.toLowerCase();
   }
 
-  console.log(compose(toUpperCase, reverse, toLowerCase)('hello world'));
-  console.log(composeRight(toLowerCase, toUpperCase, reverse)('hello world'));
+  console.log(compose(toUpperCase, reverse, toLowerCase)("hello world"));
+  console.log(composeRight(toUpperCase, reverse, toLowerCase)("hello world"));
 
+  console.log([1, 2, 3]._reduceRight((a, b) => a * b));
+  console.log([1, 2, 3].reduceRight((a, b) => a * b));
 })();
--- test35.js
+++ test35.js
@@ -1,40 +1,37 @@
-; (function () {
+(function () {
+  // https://github.com/zymfe/test-code/blob/master/tes125.js
+  var arr = [1, 2, 3, 4];
 
-	var arr = [1, 2, 3, 4];
+  // Array.prototype._reduce = function (cb, base) {
+  // 	var index = 0;
+  // 	var res = base !== undefined
+  // 		? base
+  // 		: this[index++];
+  // 	var lastIndex = this.length - 1;
+  // 	while (index <= lastIndex) {
+  // 		res = cb(res, this[index++]);
+  // 	}
+  // 	return res;
+  // }
 
-	// Array.prototype._reduce = function (cb, base) {
-	// 	var index = 0;
-	// 	var res = base !== undefined
-	// 		? base
-	// 		: this[index++];
-	// 	var lastIndex = this.length - 1;
-	// 	while (index <= lastIndex) {
-	// 		res = cb(res, this[index++]);
-	// 	}
-	// 	return res;
-	// }
+  Array.prototype._reduce = function (cb, base) {
+    var index = 0;
+    var res = base !== undefined ? base : this[index++];
+    for (; index < this.length; index++) {
+      res = cb(res, this[index]);
+    }
+    return res;
+  };
 
-	Array.prototype._reduce = function (cb, base) {
-		var index = 0;
-		var res = base !== undefined
-			? base
-			: this[index++];
-		for (; index < this.length; index++) {
-			res = cb(res, this[index]);
-		}
-		return res;
-	}
+  var sum = arr._reduce(function (a, b) {
+    return a + b;
+  }, 0);
+  console.log(sum); // 10
 
-	var sum = arr._reduce(function (a, b) {
-		return a + b;
-	}, 0);
-	console.log(sum); // 10
+  var product = arr._reduce(function (a, b) {
+    return a * b;
+  }, 1);
+  console.log(product); // 24
 
-	var product = arr._reduce(function (a, b) {
-		return a * b;
-	}, 1);
-	console.log(product); // 24
-
-	console.log([1]._reduce((a, b) => a + b)); // 1
-
+  console.log([1]._reduce((a, b) => a + b)); // 1
 })();
