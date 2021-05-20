083d4a2e792b8f838e802e72a56bbdb984080d59
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-05-20 17:42:41 +0800

    test

--- test257.js
+++ test257.js
@@ -1,10 +1,34 @@
+/**
+ * 三数之和等于某个值
+ */
 ;(function () {
 
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const arr = [6, 1, 7, 2, 3, 4, 9, 5, 6, 7, 8]
   const target = 9
 
   function sum (arr, target) {
+    arr = arr.sort((a, b) => a - b)
+
     const res = []
+    let left
+    let right
+    let fixedVal
+
+    for (let i = 0; i < arr.length; i++) {
+      fixedVal = arr[i]
+      left = i + 1
+      right = arr.length - 1
+      while (left < right) {
+        const val = arr[left] + arr[right] + fixedVal
+        if (val > target) {
+          right -= 1
+        } else if (val < target) {
+          left += 1
+        } else {
+          res.push([fixedVal, arr[left++], arr[right--]])
+        }
+      }
+    }
     
     return res
   }
